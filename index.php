73d68c1fbbb9f35f8591cc4b352f79de25435c53
arlinshiffa <sasqia.shiffa@gmail.com>
2017-11-13 23:55:21 +0700

    second commit

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 	<head>
 
 		<title>IDE</title>
-		<?php
-			include("layout/style.php");
-		?>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="lib/w3.css">
+		<link rel="stylesheet" href="lib/w3-theme-dark-grey.css">
+		<link rel="stylesheet" href="style/style.css">
+		<link rel="stylesheet" href="lib/font-awesome.min.css">
+		<link rel="stylesheet" href="lib/font-awesome.css">
 		<!-- jQuery library -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
