34915975e9060639d2d68d121dc91102f3e23134
arlinshiffa <sasqia.shiffa@gmail.com>
2017-11-14 20:07:34 +0700

    fixed layout

--- layout/header.php
+++ layout/header.php
@@ -1,7 +1,12 @@
-<html>
-    <body>
-        <div class="w3-container w3-black">
-            
+<div class="w3-container w3-theme-dark">
+        <div class="headerText">
+            <h4>Interactive Digital Learning Environtment</h4>
+            <p style="font-size:13px;">Faculty of Information Technology and Science</p>
+            <p style="font-size:13px;">Parahyangan Chatolic University</p>
+            <hr noshade>
+            <br>
         </div>
-    </body>
-</html>
+       <div width="25%">
+            <img class="w3-display-topright imgLogo" src="img/logo.png" alt="" width="130px">git
+       </div>
+</div>
---
+++ layout/sidebar.php
@@ -0,0 +1,21 @@
+<div class="w3-sidebar w3-padding-large w3-light-grey w3-text-theme-dark w3-bar-block" style="width:23%">
+        <p style="font-size:12px;">You are logged in as</p>
+        <span class=" w3-text-black" style="font-size:14px;">
+            <b>201170053</b></span><br>
+        <span class=" w3-text-black" style="font-size:14px;">
+            <b>Maria Veronica</b></span>
+          <hr noshade color="lightgrey" style="width:250px;height:5px;">
+        <img id="displaypic" src="img/profile.png" width="70px" style="border:solid;border-color:lightgrey;" alt=""> <!-- img\profile.png -->
+        <br><br>
+        <a href="#" class="w3-button">
+            <i class="fa fa-home" aria-hidden="true"></i> HOME</a><br>
+        <a href="#" class="w3-button">
+            <span></span>
+            <i class="fa fa-list" aria-hidden="true"></i> MY COURSES</a><br>
+        <a href="#" class=" w3-button">
+            <span></span>
+            <i class="fa fa-user" aria-hidden="true"></i> MY PROFILE</a><br>
+        <a href="#" class="w3-bar-item w3-button">
+            <span></span>
+            <i class="fa fa-power-off" aria-hidden="true"></i> LOG OUT</a>
+    </div>
